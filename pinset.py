d8f1d10c3d3f6fab0104cab176b6a3dd07fd5795
williamhlane <75923845+williamhlane@users.noreply.github.com>
2021-11-20 19:22:16 -0800

    Add files via upload

---
+++ pinset.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#make sure Python3 3 is installed as well as pip with sudo apt-get install python3 python3-pip
+#make sure RPi.GPIO is install with sudo pip3 install --upgrade RPi.GPIO
+#Written by William Harrison Lane III harrisonswd.com
+import RPi.GPIO as GPIO
+import sys
+if(len(sys.argv) == 1 or len(sys.argv) == 2 or len(sys.argv) > 3):
+	print("Please enter the arguments, pinset structure is \n pinset on | off PinNumber\n example pinset on 18")
+	exit()
+arg1 = str(sys.argv[1])  # on or off
+arg2 = sys.argv[2]  # pin numbers
+if(arg2.isnumeric()):
+	arg2 = int(arg2)
+	if(arg2 >= 0 and arg2 < 27):
+		if(arg1 == "on"):
+			print("Pin", str(arg2) , "has been turned", arg1 + ".")
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setwarnings(False)
+			GPIO.setup(arg2,GPIO.OUT)
+			GPIO.output(arg2,GPIO.HIGH)
+		elif(arg1 == "off"):
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setwarnings(False)
+			GPIO.setup(arg2,GPIO.OUT)
+			GPIO.output(arg2,GPIO.LOW)
+			print("Pin", str(arg2) , "has been turned", arg1 + ".")
+		else:
+			print("Please enter a valid instruction.")
+	else:
+		print("That number is out of range.")
+else:
+	print("Pin number is invalid.")
+exit()
+
